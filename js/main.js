169050ea502bf83ac5f630a159032bc27406f919
Alina <alinazimonina@gmail.com>
2019-07-03 16:03:29 +0300

    Личный проект: доверяй, но проверяй

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ var mainPin = map.querySelector('.map__pin--main');
 var adress = document.querySelector('#address');
 var fields = document.querySelectorAll('fieldset');
 
+
 for (i = 0; i < fields.length; i++) {
   fields[i].setAttribute('disabled', 'disabled');
 }
@@ -26,15 +27,15 @@ for (i = 0; i < fields.length; i++) {
 var pinMouseupHandler = function () {
   map.classList.remove('map--faded');
   forms.classList.remove('ad-form--disabled');
+  for (i = 0; i < fields.length; i++) {
+    fields[i].removeAttribute('disabled');
+  }
   renderNotes();
   addAdress();
   mainPin.removeEventListener('click', pinMouseupHandler);
 };
-
-mainPin.addEventListener('click', pinMouseupHandler);
-
 var addAdress = function () {
-  adress.value = PIN_X + '.' + PIN_Y;
+  adress.value = PIN_X + ', ' + PIN_Y;
 };
 
 var getRandomNumber = function (min, max) {
@@ -82,4 +83,74 @@ var renderNotes = function () {
   }
   similarListElement.appendChild(fragment);
 };
-addAdress();
+
+var notice = document.querySelector('.notice');
+var title = notice.querySelector('#title');
+var pricePerNight = notice.querySelector('#price');
+var type = notice.querySelector('#type');
+var timeIn = notice.querySelector('#timein');
+var timeOut = notice.querySelector('#timeout');
+
+var validateFields = function () {
+  title.setAttribute('required', 'required');
+  title.setAttribute('minlength', '30');
+  title.setAttribute('maxlength', '100');
+  pricePerNight.setAttribute('required', 'required');
+  pricePerNight.setAttribute('max', '1000000');
+};
+
+
+var typeChangeHandler = function (evt) {
+  switch (evt.target.value) {
+    case 'bungalo':
+      pricePerNight.setAttribute('min', '0');
+      pricePerNight.setAttribute('placeholder', '0');
+      break;
+    case 'flat':
+      pricePerNight.setAttribute('min', '1000');
+      pricePerNight.setAttribute('placeholder', '1000');
+      break;
+    case 'house':
+      pricePerNight.setAttribute('min', '5000');
+      pricePerNight.setAttribute('placeholder', '5000');
+      break;
+    case 'palace':
+      pricePerNight.setAttribute('min', '10000');
+      pricePerNight.setAttribute('placeholder', '10000');
+      break;
+  }
+};
+
+var timeInChangeHandler = function (evt) {
+  switch (evt.target.value) {
+    case '12:00':
+      timeOut.value = '12:00';
+      break;
+    case '13:00':
+      timeOut.value = '13:00';
+      break;
+    case '14:00':
+      timeOut.value = '14:00';
+      break;
+  }
+};
+
+var timeOutChangeHandler = function (evt) {
+  switch (evt.target.value) {
+    case '12:00':
+      timeIn.value = '12:00';
+      break;
+    case '13:00':
+      timeIn.value = '13:00';
+      break;
+    case '14:00':
+      timeIn.value = '14:00';
+      break;
+  }
+};
+
+mainPin.addEventListener('click', pinMouseupHandler);
+type.addEventListener('change', typeChangeHandler);
+timeIn.addEventListener('change', timeInChangeHandler);
+timeOut.addEventListener('change', timeOutChangeHandler);
+validateFields();
